3323eaa2cd7eaa8b01d154634445c766d3168b99
Morgai Angelina <morgai.a@phystech.edu>
2021-10-31 12:07:24 +0200

    добавлено движение шариков с отталкиванием от стены

--- catchaball.py
+++ catchaball.py
@@ -8,8 +8,13 @@ pygame.init()
 FPS = 1
 
 scores=0
-
-screen = pygame.display.set_mode((900, 900))
+#количество очков
+a=500
+#длина стороны окна
+R=50 #максимальный радиус шариков
+R0=10 #минимальный радиус шариков
+V=100 #максимальная скорость шариков
+screen = pygame.display.set_mode((a, a))
 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -19,17 +24,41 @@ MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
+t=0.1
 
 def draw_new_ball():
-    '''рисует новый шарик '''
-    global x, y, r
-    x = randint(100, 900)
-    y = randint(100, 900)
-    r = randint(10, 100)
+    '''рисует новый двигающийся шарик шарик'''
+    global x, y, r, vx, vy,t,a,R,R0,V
+    vx = randint(-V,V)
+    vy = randint(-V,V)
+    x = randint(R, a-R)
+    y = randint(R, a-R)
+    r = randint(R0, R)
     color = COLORS[randint(0, 5)]
     circle(screen, color, (x, y), r)
+    pygame.display.update()
+    for i in range(20):
+        clock.tick(30)
+        screen.fill(BLACK)
+        x += vx*t
+        y += vy*t
+        if (x-r)<0:
+            x=r
+            vx=-vx
+        elif (x+r)>(a):
+            vx=-vx
+            x = a-r
+        if (y-r)<0:
+            y=r
+            vy=-vy
+        elif (y+r)>a:
+            vy=-vy
+            y = 900-r
+        circle(screen, color, (x, y), r)
+        pygame.display.update()
+        
 def click_check():
-    '''проверяет, попал ли щелчок мышки на шарик'''
+    '''проверяет, попал ли щелчок мыши на шарик'''
     global eventx, eventy, scores
     eventx,eventy = pygame.mouse.get_pos()
     eventx=int(eventx)
@@ -37,19 +66,20 @@ def click_check():
     a = (eventx-x)**2+(eventy-y)**2
     b = (r**2)
     if a<=b:
+        clock.tick(30)
         scores+=1
         print('Click! +1 score! scores:', scores)
         screen.fill(BLACK)
         pygame.display.update()
     elif a>(b):
         print('Missed click!')
-
+        
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 
 while not finished:
-    clock.tick(FPS)
+    clock.tick(10)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
