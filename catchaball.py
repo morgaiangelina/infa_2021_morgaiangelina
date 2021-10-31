0999a9cc5dbbf238a954e960b981b14344ebf082
Morgai Angelina <morgai.a@phystech.edu>
2021-10-31 18:14:08 +0200

    Одновременное движение нескольких шариков

--- catchaball.py
+++ catchaball.py
@@ -3,6 +3,11 @@ import pygame
 from pygame.draw import *
 from random import randint
 import math
+import pygame
+import pygame
+from pygame.draw import *
+from random import randint
+import math
 pygame.init()
 
 FPS = 1
@@ -23,57 +28,64 @@ GREEN = (0, 255, 0)
 MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
+N=10
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 t=0.1
 
 def draw_new_ball():
-    '''рисует новый двигающийся шарик шарик'''
-    global x, y, r, vx, vy,t,a,R,R0,V
-    vx = randint(-V,V)
-    vy = randint(-V,V)
-    x = randint(R, a-R)
-    y = randint(R, a-R)
-    r = randint(R0, R)
-    color = COLORS[randint(0, 5)]
-    circle(screen, color, (x, y), r)
+    '''рисует новую партию двигающихся шариков'''
+    global x, y, r, vx, vy,t,a,R,R0,V,n, color
+    n=randint(2,N)
+    x = [randint(R, a-R) for i in range(n)]
+    y = [randint(R, a-R) for i in range(n)]
+    vx = [randint(-V,V) for i in range(n)]
+    vy = [randint(-V,V) for i in range(n)]
+    r = [randint(R0, R) for i in range(n)]
+    color = [COLORS[randint(0, 5)] for i in range(n)]
+    g=0
+    for i in range (n):
+        circle(screen, color[g], (x[g], y[g]), r[g])
+        g+=1
     pygame.display.update()
-    for i in range(20):
+    for i in range (100):
         clock.tick(30)
+        g=0
+        for i in range(n):
+            x[g] += vx[g]*t
+            y[g] += vy[g]*t
+            if (x[g]-r[g])<0:
+                x[g]=r[g]
+                vx[g]=-vx[g]
+            elif (x[g]+r[g])>(a):
+                vx[g]=-vx[g]
+                x[g] = a-r[g]
+            if (y[g]-r[g])<0:
+                y[g]=r[g]
+                vy[g]=-vy[g]
+            elif (y[g]+r[g])>a:
+                vy[g]=-vy[g]
+                y[g] = a-r[g]
+            g+=1
+        g=0
         screen.fill(BLACK)
-        x += vx*t
-        y += vy*t
-        if (x-r)<0:
-            x=r
-            vx=-vx
-        elif (x+r)>(a):
-            vx=-vx
-            x = a-r
-        if (y-r)<0:
-            y=r
-            vy=-vy
-        elif (y+r)>a:
-            vy=-vy
-            y = 900-r
-        circle(screen, color, (x, y), r)
+        for i in range (n):
+            circle(screen, color[g], (x[g], y[g]), r[g])
+            g+=1
         pygame.display.update()
-        
+
 def click_check():
-    '''проверяет, попал ли щелчок мыши на шарик'''
-    global eventx, eventy, scores
+    '''проверяет, попал ли щелчок мыши на какой-либо из шариков'''
+    global eventx, eventy, scores, n 
+    z=0
     eventx,eventy = pygame.mouse.get_pos()
-    eventx=int(eventx)
-    eventy=int(eventy)
-    a = (eventx-x)**2+(eventy-y)**2
-    b = (r**2)
-    if a<=b:
-        clock.tick(30)
-        scores+=1
-        print('Click! +1 score! scores:', scores)
-        screen.fill(BLACK)
-        pygame.display.update()
-    elif a>(b):
-        print('Missed click!')
-        
+    for i in range(n):
+        d = (eventx-x[z])**2+(eventy-y[z])**2
+        b = r[z]**2
+        if d<=b:
+            scores+=1
+            print('Click! +1 score! scores:', scores)
+            pygame.display.update()
+        z += 1
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
@@ -91,3 +103,4 @@ while not finished:
             
 
 pygame.quit()
+
