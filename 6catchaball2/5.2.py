237cc42156529b4aa01131c2e209df4ab136608f
Morgai Angelina <morgai.a@phystech.edu>
2021-11-15 02:05:29 +0300

    Использованы классы, исправлены предупреждения в PyCharm, осталось добавить описание функций и комментарии

--- 6catchaball2/5.2.py
+++ 6catchaball2/5.2.py
@@ -1,27 +1,17 @@
 import pygame
-import pygame
-from pygame.draw import *
+import pygame.draw as dr
 from random import randint
-import math
-import pygame
-import pygame
-from pygame.draw import *
-from random import randint
-import math
+
 pygame.init()
 
 FPS = 30
 
-scores=0
-#количество очков
-a=500
-#длина стороны окна
-R=50 #максимальный радиус шариков
-R0=10 #минимальный радиус шариков
-V=100 #максимальная скорость шариков
+a = 500
+R = 50
+R0 = 10  # минимальный радиус шариков
+V = 100  # максимальная скорость шариков
 screen = pygame.display.set_mode((a, a))
 
-w=0 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
@@ -29,156 +19,188 @@ GREEN = (0, 255, 0)
 MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
-N=10
+N = 10
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
-t=0.1
+t = 0.1
+balls = []
+donuts = []
 
 
-def draw_balls_and_donuts():
-    '''рисует новую партию двигающихся шариков и пончиков'''
-    global x, y, r, vx, vy,t,a,R,R0,V,x1, y1, r1, vx1, vy1, color, color1,n
-    n=randint(2,N)
-    x = [randint(R, a-R) for i in range(n)] # массив координат шариков по горизонтали
-    y = [randint(R, a-R) for i in range(n)] # массив координат шариков по вертикали
-    vx = [randint(-V,V) for i in range(n)] # массив горизонтальных скоростей шариков
-    vy = [randint(-V,V) for i in range(n)] # массив вертикальных скоростей шариков
-    r = [randint(R0, R) for i in range(n)] # массив радиусов шариков
-    color = [COLORS[randint(0, 5)] for i in range(n)] # массив цветов шариков
-    x1 = [randint(R, a-R) for i in range(n)] # массив координат пончиков по горизонтали
-    y1 = [randint(R, a-R) for i in range(n)] # массив координат пончиков по вертикали
-    vx1 = [randint(-V,V) for i in range(n)] # массив горизонтальных скоростей пончиков
-    vy1 = [randint(-V,V) for i in range(n)] # массив вертикальных скоростей пончиков
-    r1 = [randint(R0, R/2) for i in range(n)] # массив радиусов пончиков
-    color1 = [COLORS[randint(0, 5)] for i in range(n)] # массив цветов пончиков
-    g=0
-    for i in range (n):
-        circle(screen, color1[g], (x1[g], y1[g]), r1[g], round(r1[g]/2))
-        circle(screen, color[g], (x[g], y[g]), r[g])
-        g+=1
-    pygame.display.update()
+class Target:
+    def __init__(self, balls_additional_points=1, donuts_additional_points=5):
+        self.x = randint(R, a - R)  # массив координат шариков по горизонтали
+        self.y = randint(R, a - R)  # массив координат шариков по вертикали
+        self.vx = randint(-V, V)  # массив горизонтальных скоростей шариков
+        self.vy = randint(-V, V)  # массив вертикальных скоростей шариков
+        self.r = randint(R0, R)  # массив радиусов шариков
+        self.color = COLORS[randint(0, 5)]  # массив цветов шариков
+        self.ball_screen = screen
+        self.balls_additional_points = balls_additional_points
+        self.donuts_additional_points = donuts_additional_points
 
 
-def move_balls_and_donuts():
-    '''двигает партию двигающихся шариков и пончиков'''
-    for g in range(n):
-        x[g] += vx[g]*t
-        y[g] += vy[g]*t
-        x1[g] += randint(-V,V)*3*t
-        y1[g] += randint(-V,V)*3*t
-        if (x[g]-r[g])<0:
-            x[g]=r[g]
-            vx[g]=-vx[g]
-        elif (x[g]+r[g])>(a):
-            vx[g]=-vx[g]
-            x[g] = a-r[g]
-        if (y[g]-r[g])<0:
-            y[g]=r[g]
-            vy[g]=-vy[g]
-        elif (y[g]+r[g])>a:
-            vy[g]=-vy[g]
-            y[g] = a-r[g]
-        if (x1[g]-r1[g])<0:
-            x1[g]=r1[g]
-        elif (x1[g]+r1[g])>(a):
-            x1[g] = a-r1[g]
-        if (y1[g]-r1[g])<0:
-            y1[g]=r1[g]
-        elif (y1[g]+r1[g])>a:
-            y1[g] = a-r1[g]
-    screen.fill(BLACK)
-    for g in range (n):
-        circle(screen, color1[g], (x1[g], y1[g]), r1[g], round(r1[g]/2))
-        circle(screen, color[g], (x[g], y[g]), r[g])
-    pygame.display.update()
+class Ball(Target):
+    def move(self):
+        self.x += self.vx * t
+        self.y += self.vy * t
+        if (self.x - self.r) < 0:
+            self.x = self.r
+            self.vx = -self.vx
+        elif (self.x + self.r) > a:
+            self.vx = -self.vx
+            self.x = a - self.r
+        if (self.y - self.r) < 0:
+            self.y = self.r
+            self.vy = -self.vy
+        elif (self.y + self.r) > a:
+            self.vy = -self.vy
+            self.y = a - self.r
 
-def click_check():
-    '''проверяет, попал ли щелчок мыши на какой-либо из шариков или пончиков'''
-    global eventx, eventy, scores, n,w 
-    eventx,eventy = pygame.mouse.get_pos()
-    for i in range(n):
-        d = (eventx-x[i])**2+(eventy-y[i])**2
-        b = r[i]**2
-        d1 = (eventx-x1[i])**2+(eventy-y1[i])**2
-        b1 = r1[i]**2
-        c1 = (r1[i]**2)/4
-        if d<=b:
-            scores+=1
-            w+=1
-            print('A ball is catched! +1 score! scores:', scores)
-            x[i] = randint(R, a-R)
-            y[i] = randint(R, a-R)
-            vx[i] = randint(-V,V)
-            vy[i] = randint(-V,V)
-            r[i] = randint(R0, R)
-            color[i] = COLORS[randint(0, 5)]
-        if d1<=b1:
-            if d1>=c1:
-                scores+=5
-                w+=5
-                print('A donut is catched! +5 score! scores:', scores)
-                x1[i] = randint(R, a-R)
-                y1[i] = randint(R, a-R)
-                vx1[i] = randint(-V,V)
-                vy1[i] = randint(-V,V)
-                r1[i] = randint(R0, R)
-                color1[i] = COLORS[randint(0, 5)]
+    def draw(self):
+        """Функция рисует мяч"""
+        dr.circle(screen, self.color, (self.x, self.y), self.r)
 
-def save_scores():
-    '''создаёт файл с упорядоченным набором очков игроков'''
-    global finished
-    f = open('scores.txt','a')
-    f.write(name_of_player+'\n')
-    f.write(str(scores)+'\n')
-    f.close()
-    j=0
-    SCORES = []
-    NAMES = []
-    f = open('scores.txt','r')
-    while True:
-        line = f.readline()
-        if not line:
-            break
-        if (-1)**j==1:
-            DATA0 = str(line)
-            DATA0.replace('\n','')
-            NAMES.append(DATA0)
-        elif (-1)**j==-1:
-            DATA = str(line)
-            DATA.replace('\n','')
-            DATA = int(DATA)
-            SCORES.append(DATA)
-        j+=1                
-    f.close()
-    f = open('scores.txt','w')
-    while max(SCORES)>-1:
-        f.write(NAMES[SCORES.index(max(SCORES))])
-        f.write(str(SCORES[SCORES.index(max(SCORES))])+'\n')
-        SCORES[SCORES.index(max(SCORES))]=-1
-    f.close()
-    finished = True
-        
+    def hittest(self, plus_scores, live):
+        eventx, eventy = pygame.mouse.get_pos()
+        if ((eventx - self.x) ** 2 + (eventy - self.y) ** 2) <= (self.r ** 2):
+            plus_scores += self.balls_additional_points
+            live += self.balls_additional_points
+            print('A ball is catched! +', self.balls_additional_points, ' score! scores:', plus_scores)
+            self.x = randint(R, a - R)
+            self.y = randint(R, a - R)
+            self.vx = randint(-V, V)
+            self.vy = randint(-V, V)
+            self.r = randint(R0, R)
+            self.color = COLORS[randint(0, 5)]
+        return plus_scores, live
+
+
+class Donut(Target):
+    def move(self):
+        self.x += randint(-V, V) * 3 * t
+        self.y += randint(-V, V) * 3 * t
+        if (self.x - self.r) < 0:
+            self.x = self.r
+        elif (self.x + self.r) > a:
+            self.x = a - self.r
+        if (self.y - self.r) < 0:
+            self.y = self.r
+        elif (self.y + self.r) > a:
+            self.vy = -self.vy
+            self.y = a - self.r
+
+    def draw(self):
+        """Функция рисует мяч"""
+        dr.circle(screen, self.color, (self.x, self.y), self.r, round(self.r / 2))
+
+    def hittest(self, plus_scores, live):
+        eventx, eventy = pygame.mouse.get_pos()
+        if ((eventx - self.x) ** 2 + (eventy - self.y) ** 2) <= (self.r ** 2):
+            if ((eventx - self.x) ** 2 + (eventy - self.y) ** 2) >= ((self.r ** 2) / 4):
+                plus_scores += self.donuts_additional_points
+                live += self.donuts_additional_points
+                print('A ball is catched! +', self.donuts_additional_points, ' score! scores:', plus_scores)
+                self.x = randint(R, a - R)
+                self.y = randint(R, a - R)
+                self.vx = randint(-V, V)
+                self.vy = randint(-V, V)
+                self.r = randint(R0, round(R/2))
+                self.color = COLORS[randint(0, 5)]
+        return plus_scores, live
+
+
+class MainFunctions:
+    def __init__(self, scores=0, max_number_of_one_type_objects=10, live_points=0):
+        self.live_points = live_points
+        self.scores = scores
+        self.max_number_of_one_type_objects = max_number_of_one_type_objects
+
+    def generate_array_of_objects(self, array_balls, array_donuts):
+        number_of_balls = randint(2, self.max_number_of_one_type_objects)
+        for i in range(number_of_balls):
+            new_ball = Ball()
+            array_balls.append(new_ball)
+        number_of_donuts = randint(2, self.max_number_of_one_type_objects)
+        for i in range(number_of_donuts):
+            new_donut = Donut()
+            array_donuts.append(new_donut)
+
+    def draw_array_of_objects(self):
+        screen.fill(BLACK)
+        for i in balls:
+            i.draw()
+        for i in donuts:
+            i.draw()
+
+    def move_array_of_objects(self):
+        for i in balls:
+            i.move()
+        for i in donuts:
+            i.move()
+
+    def click_check(self):
+        for i in balls:
+            self.scores, self.live_points = i.hittest(self.scores, self.live_points)
+        for i in donuts:
+            self.scores, self.live_points = i.hittest(self.scores, self.live_points)
+
+    def save_scores(self):
+        """создаёт файл с упорядоченным набором очков игроков"""
+        results_file = open('scores.txt', 'a')
+        results_file.write(name_of_player + '\n')
+        results_file.write(str(game.scores) + '\n')
+        results_file.close()
+        counting_variable = 0
+        scores_statistics = []
+        names_data = []
+        results_file = open('scores.txt', 'r')
+        while True:
+            line = results_file.readline()
+            if not line:
+                break
+            if (-1) ** counting_variable == 1:
+                name = str(line)
+                name.replace('\n', '')
+                names_data.append(name)
+            elif (-1) ** counting_variable == -1:
+                number_of_scores = str(line)
+                number_of_scores.replace('\n', '')
+                number_of_scores = int(number_of_scores)
+                scores_statistics.append(number_of_scores)
+            counting_variable += 1
+        results_file.close()
+        results_file = open('scores.txt', 'w')
+        while max(scores_statistics) > -1:
+            results_file.write(names_data[scores_statistics.index(max(scores_statistics))])
+            results_file.write(str(scores_statistics[scores_statistics.index(max(scores_statistics))]) + '\n')
+            scores_statistics[scores_statistics.index(max(scores_statistics))] = -1
+        results_file.close()
+
+
+finished = False
 pygame.display.update()
 clock = pygame.time.Clock()
-finished = False
-
-
 print('Введите имя игрока:')
 name_of_player = input()
-draw_balls_and_donuts()
+
+game = MainFunctions()
+
+game.generate_array_of_objects(balls, donuts)
+game.draw_array_of_objects()
 
 while not finished:
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            save_scores()
+            game.save_scores()
+            finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            click_check()
-    if w>=10:
-        draw_balls_and_donuts()
-        w=0
-    move_balls_and_donuts()        
+            game.click_check()
+    if game.live_points >= 10:
+        game.generate_array_of_objects(balls, donuts)
+        game.live_points = 0
+    game.move_array_of_objects()
+    game.draw_array_of_objects()
     pygame.display.update()
 
-
 pygame.quit()
-
